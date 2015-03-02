3aab4e29bec18a7412107503e480994714da7613
heller@H-L-PC <heller@zju.edu.cn>
2015-03-03 00:35:04 +0800

    initial project ch02-demo1

---
+++ mybatis-wx-robot-basic/ch02-demo1/src/main/java/com/imooc/servlet/ListServlet.java
@@ -0,0 +1,33 @@
+package com.imooc.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * 列表页面初始化控制
+ * 
+ * @author heller
+ *
+ */
+@SuppressWarnings("serial")
+@WebServlet("/list.action")
+public class ListServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		// 直接跳转到list页面
+		req.getRequestDispatcher("/WEB-INF/jsp/back/list.jsp").forward(req, resp);
+	}
+	
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		this.doGet(req, resp);
+	}
+}
