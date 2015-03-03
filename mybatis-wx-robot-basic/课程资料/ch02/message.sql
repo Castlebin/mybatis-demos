a4e6a1999d153163802297e6e66688b611fd01a6
heller@H-L-PC <heller@zju.edu.cn>
2015-03-03 23:42:47 +0800

    update log4j settings and the sql

--- mybatis-wx-robot-basic/课程资料/ch02/message.sql
+++ mybatis-wx-robot-basic/课程资料/ch02/message.sql
@@ -1,11 +1,13 @@
-﻿CREATE TABLE `message` (
+use mybatis;
+
+DROP TABLE IF EXISTS `message`;
+CREATE TABLE `message` (
   `ID` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `COMMAND` varchar(16) DEFAULT NULL COMMENT '指令名称',
   `DESCRIPTION` varchar(32) DEFAULT NULL COMMENT '描述',
   `CONTENT` varchar(2048) DEFAULT NULL COMMENT '内容',
   PRIMARY KEY (`ID`)
-) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8;
-
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 INSERT INTO `message` VALUES (1, '查看', '精彩内容', '精彩内容');
 INSERT INTO `message` VALUES (2, '段子', '精彩段子', '如果你的月薪是3000块钱，请记得分成五份，一份用来买书，一份给家人，一份给女朋友买化妆品和衣服，一份请朋友们吃饭，一份作为同事的各种婚丧嫁娶的份子钱。剩下的2999块钱藏起来，不要告诉任何人');
@@ -13,3 +15,4 @@ INSERT INTO `message` VALUES (3, '新闻', '今日头条', '7月17日，马来�
 INSERT INTO `message` VALUES (4, '娱乐', '娱乐新闻', '昨日，邓超在微博分享了自己和孙俪的书法。夫妻同样写幸福，但差距很大。邓超自己都忍不住感慨字丑：左边媳妇写的。右边是我写的。看完我再也不幸福了。');
 INSERT INTO `message` VALUES (5, '电影', '近日上映大片', '《忍者神龟》[2]真人电影由美国派拉蒙影业发行，《洛杉矶之战》导演乔纳森·里贝斯曼执导。 \r\n片中四只神龟和老鼠老师都基于漫画和卡通重新绘制，由动作捕捉技术实现。\r\n其中皮特·普劳泽克饰演达芬奇(武器：武士刀)，诺尔·费舍饰演米开朗基罗(武器：双节棍)，阿伦·瑞奇森饰演拉斐尔(武器：铁叉)，杰瑞米·霍华德饰演多拉泰罗(武器：武士棍)。\r\n该片计划于2014年8月8日在北美上映。');
 INSERT INTO `message` VALUES (6, '彩票', '中奖号码', '查啥呀查，你不会中奖的！');
+
